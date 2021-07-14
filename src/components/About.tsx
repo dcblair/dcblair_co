a94244e2bfebaa0fd8ea1351651c948513fe5a6b
dcblair <devin.c.blair@gmail.com>
2021-07-13 22:27:43 -0700

    Convert About to TS.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Card, makeStyles, Typography } from '@material-ui/core';
-import React, { FC } from 'react';
+import React from 'react';
 import Profile from '../assets/profile_500.png';
 import FadeSpring from './FadeSpring';
 import FontawesomeButton from './FontawesomeButton';
@@ -61,7 +61,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const About: FC = () => {
+const About = () => {
   const classes = useStyles();
 
   return (
